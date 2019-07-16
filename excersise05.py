0a000dbae7a0450f4bab194ea97151a4b6574a1c
Kayofpimentel <kayofpimentel@gmail.com>
2019-07-16 12:14:32 -0300

    Changes made in Module 6.

---
+++ excersise05.py
@@ -0,0 +1,16 @@
+import random
+import time
+
+rand = random.random()
+rand10 = (9*random.random()) + 1
+
+print(rand)
+print(rand10)
+
+micro = time.time()
+
+print(micro)
+
+unique = rand10 * micro
+
+print(unique)
